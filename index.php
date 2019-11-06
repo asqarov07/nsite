329e7c1d20a71a0dce7a0658b75de53d2d721837
Asqarov Yeldos <32400536+asqarov07@users.noreply.github.com>
2019-11-06 22:00:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -612,7 +612,7 @@
       <section class="section section-sm section-first bg-default">
         <div class="container">
           <h3 class="heading-3">ЗАКАЗАТЬ</h3>
-          <form class="rd-form rd-mailform form-style-1" data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/telega.php"> <!--bat/rd-mailform.php-->
+          <form class="rd-form rd-mailform form-style-1" data-form-output="form-output-global" data-form-type="contact" method="post" action="telega.php"> <!--bat/rd-mailform.php-->
             <div class="row row-20 gutters-20">
               <div class="col-md-6 col-lg-4 oh-desktop">
                 <div class="form-wrap wow slideInDown">
